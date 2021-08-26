86eccc3b344175b1fcf15a29c7785773a573af45
sharad9201 <sharad.rae19@gmail.com>
2021-08-26 15:46:51 +0545

    image obtained

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    
+    function addProduct(Request $request){
+     
+        $product = new Product;
+       
+        $product->name=$request->input('name');
+        $product->price=$request->input('price');
+        $product->description = $request->input('description');
+        $product->file_path = $request->file('file')->store('products');
+        $product->save();
+        return $product;
+    }
+}
